70a6cdd3722adaf7afb3e20e5e6d36cc209dba2a
SoftwareShenzheneer <tj.hooij@outlook.com>
2025-12-25 10:03:49 +0800

    Added obsidian for daily notes

--- lua/plugins/nvim-telescope.lua
+++ lua/plugins/nvim-telescope.lua
@@ -5,6 +5,16 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       require("telescope").setup({
+        defaults = {
+          file_ignore_patterns = {
+            "docs/*",
+            "tools/*",
+            "prebuilt/*",
+            "mbedtls/*",
+            "build/*",
+            -- "out/*"
+          }
+        },
         extensions = {
           ["ui-select"] = {
             require("telescope.themes").get_dropdown({}),
