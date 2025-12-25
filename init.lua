633c786bf0d0189b4bebc9368a8d577bcb8fe7f0
SoftwareShenzheneer <tj.hooij@outlook.com>
2025-12-25 10:03:53 +0800

    Removed not needed plugins, restructured keybindings, change: swapped telescope for fzf
    feature: added undo history support to init file

--- init.lua
+++ init.lua
@@ -1,21 +1,37 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-	if vim.v.shell_error ~= 0 then
-		vim.api.nvim_echo({
-			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-			{ out, "WarningMsg" },
-			{ "\nPress any key to exit..." },
-		}, true, {})
-		vim.fn.getchar()
-		os.exit(1)
-	end
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out, "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
 
 vim.opt.rtp:prepend(lazypath)
 require("remap")
 require("lazy").setup("plugins")
 -- require("dev.myplugin").setup()
+
+-- Determine undo directory inside stdpath('data')
+local undo_dir = vim.fn.stdpath('data') .. '/undo_history'
+
+-- Create the directory if it doesn't exist
+if vim.fn.isdirectory(undo_dir) == 0 then
+  vim.fn.mkdir(undo_dir, 'p')
+end
+
+-- Set undo options
+vim.opt.undodir = undo_dir
+vim.opt.undofile = true
+
+-- Optional: increase undo history limits
+vim.opt.undolevels = 1000
+vim.opt.undoreload = 1000
 
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,23 +1,8 @@
-function colourTheme(colour)
-  colour = colour or "tokyonight"
-  vim.cmd.colorscheme(colour)
-end
-
 return {
-  -- colourTheme()
-  -- "askfiy/visual_studio_code",
-  -- priority = 100,
-  -- config = function()
-  --   vim.cmd([[colorscheme visual_studio_code]])
-  --   vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-  --   vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-  -- end,
-
   "folke/tokyonight.nvim",
   lazy = false;
   opts = {},
   config = function()
-      colourTheme()
       local colour = colour or "tokyonight-storm"
       vim.cmd.colorscheme(colour)
       vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
