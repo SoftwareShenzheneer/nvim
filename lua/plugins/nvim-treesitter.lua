33ed6da5193ef3350d75a7235059f96ab70ddb67
SoftwareShenzheneer <tj.hooij@outlook.com>
2025-12-25 03:38:44 +0000

    Change: removed deprecated treesitter/playgrounds

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -16,33 +16,6 @@ return {
     end,
   },
   {
-    "nvim-treesitter/playground",
-    config = function()
-      local configs = require("nvim-treesitter.configs")
-      configs.setup({
-        playground = {
-          enable = true,
-          disable = {},
-          updatetime = 25,
-          persist_queries = false,
-          keybindings = {
-            toggle_query_editor = 'o',
-            toggle_hl_groups = 'i',
-            toggle_injected_languages = 't',
-            toggle_anonymous_nodes = 'a',
-            toggle_language_display = 'I',
-            focus_language = 'f',
-            unfocus_language = 'F',
-            update = 'R',
-            goto_node = '<cr>',
-            show_help = '?',
-          },
-        }
-      })
-    end,
-  },
-  {
-    
     "nvim-treesitter/nvim-treesitter-context",
     config = function()
       require("treesitter-context").setup({
